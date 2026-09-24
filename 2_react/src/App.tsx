2426203fd8db6cd479ec78d69723b0f77cda71ca
Ni-then <n1th3n.11@gmail.com>
2026-09-24 20:45:14 +0530

    dependecy-array

--- 2_react/src/App.tsx
+++ 2_react/src/App.tsx
@@ -1,28 +1,49 @@
-import axios from 'axios'
-import React, { useState ,useEffect} from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-  // data is a state variable ,so jb jb state var change hoga , component re-render hoga 
-  // Note : only first time this component is rendered , will data be intialized to [] , in all futute re-renders , it will not get re-initalized
-  const [data,setData] = useState([])
-  // if the dependency array (2nd argument) is empty , so this function only runs on the first render (mount)
-  useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/todos")
-    .then(res=>setData(res.data))
-  },[])
+  const [todo, setTodo] = useState(1); //state variable
+
+  return (
+    <div className=''>
+      <button className='bg-red-400 rounded-xl ml-2 p-2' onClick={() => setTodo(1)}>1</button> 
+      {/* first time i click on the btn 1 , so control reach to the Render(props= todo --> 1) then useEffect calls but i use empty dependency array means its only run on first render (mount) on second , third--- render it will not run */}
+      {/* SOl : pass the data (state variable) in the useEffect dependency array which means jb jb mera state variable (data) change hoga tb tb useEffect call hoga , means tb tb re-render hoga */}
+      <button className='bg-red-400 rounded-xl ml-2 p-2' onClick={() => setTodo(2)}>2</button>
+      <button className='bg-red-400 rounded-xl ml-2 p-2' onClick={() => setTodo(3)}>3</button>
+      <button className='bg-red-400 rounded-xl ml-2 p-2' onClick={() => setTodo(4)}>4</button>
+
+      <Render id={todo} />
+    </div>
+  );
+};
+
+function Render(props) {
+  const [data, setData] = useState(null);
+  const todoId = props.id;
+
+  useEffect(() => {
+    console.log("inside useEffect" + todoId)
+    axios
+      .get(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+      .then((res) => setData(res.data));
+      //clean up function
+      return function(){
+        console.log("hi there" + todoId)
+      }
+
+    return function(){
+
+    }
+  }, [todoId]);
+
   return (
     <div>
-      {data.map((p)=><Todo title = {p.title}/>)}
+        <div>
+          <h2>{data?.title}</h2>
+        </div>
     </div>
-  )
+  );
 }
 
-function Todo(props){
-  return (
-    <div>
-      {props.title}
-    </div>
-  )
-}
-
-export default App
+export default App;
