8b12b2bb91d0dce77724db32cde9305e5f10a8d4
Ni-then <n1th3n.11@gmail.com>
2026-09-26 21:16:36 +0530

    add

--- 4_react/src/topic/timer-problem.jsx
+++ 4_react/src/topic/timer-problem.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useRef, useState } from 'react'
 
 const App = () => {
     const [timer, setTimer] = useState(0)
-    const [interval, setIntervalVarible] = useState()
+    // const [interval, setIntervalVarible] = useState()
+    let interval = useRef(0)
     // problem : 
     // timer and setTimer yeh state variable hai, by default timer ki value 0 hai
     // btn click krne pr function chalega
@@ -13,7 +14,8 @@ const App = () => {
     // but problem is mera abhi bhe old setInterval, chal raha hai, jiske under timer ki old value hai whihc is 0, na ki 1 
     // is liye timer is stuck on 1 second ??
     function startWatch() {
-        let i = setInterval(() => {
+        // interval.current , this is almost behaving like the global variable , it does not cause the re-render
+        interval.current= setInterval(() => {
             // setTimer(timer+1) // this not ??
             setTimer((s) => { // s : latest variable ki state hoti hai
                 return s + 1;
@@ -21,11 +23,12 @@ const App = () => {
             // or
             // setTimer(s=>s+1)
         }, 1000)
-        setIntervalVarible(i)
+        // setIntervalVarible(i)
         // my setInterval is working on every one second , but some how this timer is not changed or it just stuck at 1 second
     }
     function stopWatch() {
-        clearInterval(interval)
+        // clearInterval(interval)
+        clearInterval(interval.current)
 
     }
     // whole problem : jaise jaise timer ki value update ho rahi hai means 1, 2, 3 ,4 ---- so har baar mera component re-render ho raha hai
